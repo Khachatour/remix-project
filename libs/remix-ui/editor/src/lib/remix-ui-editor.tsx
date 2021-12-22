ca0f124c25b73cd8483e8ac1ac750bf672ead2cf
lianahus <liana@ethereum.org>
2021-12-22 16:37:17 +0100

    fixed the colors of editor's content menu

--- libs/remix-ui/editor/src/lib/remix-ui-editor.tsx
+++ libs/remix-ui/editor/src/lib/remix-ui-editor.tsx
@@ -209,7 +209,12 @@ export const EditorUI = (props: EditorUIProps) => {
         'editor.lineHighlightBorder': secondaryColor,
         'editor.lineHighlightBackground': textbackground === darkColor ? lightColor : secondaryColor,
         'editorGutter.background': lightColor,
-        'minimap.background': lightColor
+        'minimap.background': lightColor,
+        'menu.foreground': textColor,
+        'menu.background': textbackground,
+        'menu.selectionBackground': secondaryColor,
+        'menu.selectionForeground': textColor,
+        'menu.selectionBorder': secondaryColor
       }
     })
     monacoRef.current.editor.setTheme(themeName)
